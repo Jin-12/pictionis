a0af0f2f06ca1ee4d76fc2cfd2486618d3c41f6b
Jules Couturaud <coutur_j@etna-alternance.net>
2022-11-07 00:31:23 +0100

    add ui elements to slide up panel for chat room

--- pictionis/lib/screens/game_screen.dart
+++ pictionis/lib/screens/game_screen.dart
@@ -39,8 +39,75 @@ class _GameScreenState extends State<GameScreen> {
     return Scaffold(
       backgroundColor: Colors.yellow[100],
       body: SlidingUpPanel(
-        panel: const Center(
-          child: Text("This is the sliding Widget."),
+        backdropEnabled: true,
+        panel: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
+                height: 60,
+                width: double.infinity,
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        height: 30,
+                        width: 30,
+                        decoration: BoxDecoration(
+                          color: Colors.lightBlue,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: const Icon(
+                          Icons.add,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    const Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                            hintText: "Write message...",
+                            hintStyle: TextStyle(color: Colors.black54),
+                            border: InputBorder.none),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    FloatingActionButton(
+                      onPressed: () {},
+                      backgroundColor: Colors.blue,
+                      elevation: 0,
+                      child: const Icon(
+                        Icons.send,
+                        color: Colors.white,
+                        size: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        collapsed: Container(
+          color: Colors.blueGrey,
+          child: const Center(
+            child: Text(
+              "CHAT",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+          ),
         ),
         body: Stack(
           children: [
