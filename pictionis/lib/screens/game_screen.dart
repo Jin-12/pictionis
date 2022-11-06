33136e5e0a073ea30963f785eb081bdabc4ccacf
Jules Couturaud <coutur_j@etna-alternance.net>
2022-11-06 23:22:36 +0100

    Canva UI with CustomPaint Widget and Slide up panel for chatbox

--- pictionis/lib/screens/game_screen.dart
+++ pictionis/lib/screens/game_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+import '../main.dart';
+import 'package:pictionis/src/drawn_line.dart';
+import 'package:pictionis/src/sketcher.dart';
 import 'package:flutter/material.dart';
-import 'package:pictionis/main.dart';
-import 'package:provider/provider.dart';
 
 class GameScreen extends StatefulWidget {
   static String routeName = '/game';
@@ -11,19 +16,195 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
+  final GlobalKey _globalKey = new GlobalKey();
+  DrawnLine? line;
+  List<DrawnLine> lines = <DrawnLine>[];
+  Color selectedColor = Colors.black;
+  double selectedWidth = 5.0;
+
+  StreamController<List<DrawnLine>> linesStreamController =
+      StreamController<List<DrawnLine>>.broadcast();
+  StreamController<DrawnLine> currentLineStreamController =
+      StreamController<DrawnLine>.broadcast();
+
+  Future<void> clear() async {
+    setState(() {
+      lines = [];
+      line = null;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      margin: const EdgeInsets.symmetric(
-        horizontal: 20,
+      backgroundColor: Colors.yellow[100],
+      body: SlidingUpPanel(
+        panel: const Center(
+          child: Text("This is the sliding Widget."),
+        ),
+        body: Stack(
+          children: [
+            buildAllPaths(context),
+            buildCurrentPath(context),
+            buildColorToolbar(),
+            buildStrokeToolbar(),
+          ],
+        ),
       ),
+    );
+  }
+
+  GestureDetector buildCurrentPath(BuildContext context) {
+    return GestureDetector(
+      onPanStart: onPanStart,
+      onPanUpdate: onPanUpdate,
+      onPanEnd: onPanEnd,
+      child: RepaintBoundary(
+        child: Container(
+          color: Colors.transparent,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: StreamBuilder<DrawnLine>(
+            stream: currentLineStreamController.stream,
+            builder: (context, snapshot) {
+              return CustomPaint(
+                painter: Sketcher(
+                  lines: [line],
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildAllPaths(BuildContext context) {
+    return RepaintBoundary(
+      key: _globalKey,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: StreamBuilder<List<DrawnLine>>(
+          stream: linesStreamController.stream,
+          builder: (context, snapshot) {
+            return CustomPaint(
+              painter: Sketcher(
+                lines: lines,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  void onPanStart(DragStartDetails details) {
+    print('User started drawing');
+    final box = context.findRenderObject() as RenderBox;
+    final point = box.globalToLocal(details.globalPosition);
+    line = DrawnLine([point], selectedColor, selectedWidth);
+    currentLineStreamController.add(line!);
+  }
+
+  void onPanUpdate(DragUpdateDetails details) {
+    final box = context.findRenderObject() as RenderBox;
+    final point = box.globalToLocal(details.globalPosition);
+    final path = List<Offset?>.from(line!.path)..add(point);
+    line = DrawnLine(path, selectedColor, selectedWidth);
+    currentLineStreamController.add(line!);
+  }
+
+  void onPanEnd(DragEndDetails details) {
+    lines = List.from(lines)..add(line!);
+    linesStreamController.add(lines);
+  }
+
+  Widget buildStrokeToolbar() {
+    return Positioned(
+      bottom: 100.0,
+      right: 10.0,
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Text(Provider.of<ApplicationState>(context).roomData.toString())
+          buildStrokeButton(5.0),
+          buildStrokeButton(10.0),
+          buildStrokeButton(15.0),
         ],
       ),
-    ));
+    );
+  }
+
+  Widget buildStrokeButton(double strokeWidth) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedWidth = strokeWidth;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Container(
+          width: strokeWidth * 2,
+          height: strokeWidth * 2,
+          decoration: BoxDecoration(
+              color: selectedColor, borderRadius: BorderRadius.circular(50.0)),
+        ),
+      ),
+    );
+  }
+
+  Widget buildColorToolbar() {
+    return Positioned(
+      top: 40.0,
+      right: 10.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          buildColorButton(Colors.red),
+          buildColorButton(Colors.blueAccent),
+          buildColorButton(Colors.deepOrange),
+          buildColorButton(Colors.green),
+          buildColorButton(Colors.lightBlue),
+          buildColorButton(Colors.black),
+          buildColorButton(Colors.white),
+          const Divider(
+            height: 50.0,
+          ),
+          buildClearButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildColorButton(Color color) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: FloatingActionButton(
+        mini: true,
+        backgroundColor: color,
+        child: Container(),
+        onPressed: () {
+          setState(() {
+            selectedColor = color;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget buildClearButton() {
+    return GestureDetector(
+      onTap: clear,
+      child: const CircleAvatar(
+        child: Icon(
+          Icons.create,
+          size: 20.0,
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
